bd3799af5a7ad56a3e9a0b763c0566c98d21eded
buschidos <sergey.snjozyk@gmail.com>
2016-01-19 14:53:56 +0200

    add initial fade in animations

--- app/js/app.js
+++ app/js/app.js
@@ -45,12 +45,14 @@ morhOnvif();
 morphLightning();
 initTextareaAutoresize();
 buildHeaderScrollScene();
+buildContentFadeScenes();
 buildLegoScene();
-setFeaturesHeight();
+setTimeout(setFeaturesHeight, 200);
 initMap('#map');
 svg4everybody();
+drawHeroSvg('.hero-figure.is-active svg', 300);
 
-function drawHeroSvg(el, cb = $.noop) {
+function drawHeroSvg(el, timeout = 0, cb = $.noop) {
     if (mq.matches) return;
 
     if (typeof el === 'string') {
@@ -69,7 +71,9 @@ function drawHeroSvg(el, cb = $.noop) {
         callback: cb
     });
 
-    el.drawsvg('animate');
+    setTimeout(() => {
+        el.drawsvg('animate');
+    }, timeout);
 }
 
 function toggleSection(e) {
@@ -98,7 +102,7 @@ function toggleSection(e) {
 
     setFeaturesHeight(targetFeatures);
 
-    drawHeroSvg(targetFigure.find('svg'), () => {
+    drawHeroSvg(targetFigure.find('svg'), null, () => {
         targetFeatures.parent().removeClass('is-changing');
         dispatcher.trigger(actions.END_CHANGE_SECTION, sectionName);
     });
@@ -222,4 +226,48 @@ function buildLegoScene() {
     }).on('end', (e) => {
         container.toggleClass('animation-done');
     }).addTo(scrollController);
+}
+
+function buildContentFadeScenes() {
+    const sections = $('.fade-in');
+    const duration = 500;
+    const stack = [];
+
+    const debouncedAnimate = debounce(() => {
+        stack.forEach((obj, i) => {
+            let delay = 150 * i;
+            delay = delay > 450 ? 450 : delay;
+
+            if (obj.init) return;
+
+            obj.init = true;
+
+            setTimeout(() => {
+                obj.$el.addClass('animate');
+            }, delay);
+
+            setTimeout(() => {
+                obj.$el.removeClass('fade-in slide-up animate');
+            }, delay + duration);
+        });
+        stack.length = 0;
+    }, 100);
+
+    sections.each((index, section) => {
+        let $section = $(section);
+        let offset = $section.hasClass('slide-up') ? 100 : 50;
+
+        let scene = new ScrollMagic.Scene({
+            offset,
+            triggerElement: section,
+            triggerHook: 'onEnter'
+        }).on('start', () => {
+            stack.push({
+                $el: $section,
+                init: false
+            });
+            debouncedAnimate();
+            scene.destroy();
+        }).addTo(scrollController);
+    });
 }

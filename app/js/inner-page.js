28b57a6690efa8a4c27446175abfd6b28143c776
hraboviyvadim <hraboviyvadim@gmail.com>
2016-02-23 13:53:37 +0200

    about page

--- app/js/inner-page.js
+++ app/js/inner-page.js
@@ -1,6 +1,34 @@
 import $ from 'jquery';
 window.$ = window.jQuery = $;
+import ScrollMagic from 'scrollmagic';
 import buildContentFadeScenes from './fade-in-scenes.js';
-console.log($);
+import svg4everybody from 'svg4everybody';
 
-buildContentFadeScenes()
+buildContentFadeScenes();
+svg4everybody();
+
+$('.menu-button').on('click touchend', function(e) {
+    e.preventDefault();
+    $(this).parents('.header').toggleClass('is-show-menu');
+});
+
+function innerHeaderScroll() {
+
+    var controller = new ScrollMagic.Controller({
+        container: 'body',
+        loglevel: 2
+    });
+
+	var header = $('.header');
+
+	const scene = new ScrollMagic.Scene({
+	    offset: 100,
+	    triggerElement: 'body',
+	    triggerHook: 'onLeave'
+	}).on('start', () => {
+	    header.toggleClass('scrolled');
+	}).addTo(controller);
+
+}
+
+innerHeaderScroll();
